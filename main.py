17d40ecc7629ca0c875678c22e8976028d452898
link033 <054656-106144@stu.mlc.edu.tw>
2021-09-26 07:33:42 +0800

    戰機移動及更改背景顏色

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ class main(Canvas):
     def __init__(self,width=1000,height=700):
         self.root=Tk()
         self.root.title('shoot-the-evemy')
-        super(main,self).__init__(self.root,width=width,height=height,bg='#ffaaff')
+        super(main,self).__init__(self.root,width=width,height=height,bg='#000000')
         self.pack()
 
         time.sleep(1)
@@ -22,16 +22,20 @@ class main(Canvas):
         self.y=0
         self.but1=0
 
-        while True:
+        while self.cover.delete==0:
             self.cover.loop(self.x,self.y,self.but1)
-
-            if self.cover.delete==1:
-                self.start()
 
             self.root.update()
             time.sleep(0.01)
             if self.but1==1:
                 self.but1==0
+
+        self.start()
+        while True:
+            self.player.loop(self.x,self.y)
+
+            self.root.update()
+            time.sleep(0.01)
 
         self.root.mainloop()
 
@@ -46,7 +50,7 @@ class main(Canvas):
 
     def start(self):
         self.player_image=PhotoImage(file='platyer_image.png')
-        self.player.start(self.player_image)
+        self.player.start(self.player_image,self.root,time)
 
 if __name__=='__main__':
     main()
--- player/player.py
+++ player/player.py
@@ -2,5 +2,17 @@ class player:
     def __init__(self,canvas):
         self.canvas=canvas
 
-    def start(self,item):
-        self.item=self.canvas.create_image(500,640,image=item)
+    def start(self,item,root,time):
+        self.item=self.canvas.create_image(500,800,image=item)
+        for t in range(60):
+            self.canvas.move(self.item,0,-5)
+            root.update()
+            time.sleep(0.01)
+
+    def loop(self,x,y):
+        self.xy=self.canvas.coords(self.item)
+        if abs(x-self.xy[0])>3:
+            if self.xy[0]>x:
+                self.canvas.move(self.item,-3,0)
+            else:
+                self.canvas.move(self.item,3,0)
